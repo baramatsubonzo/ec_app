788c591a008d04832bed5334d689439b70cae52b
baramatsubonzo <baramatsubonzo@gmail.com>
2018-09-07 09:50:14 +0900

    認証機能の追加とトップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  #自分で追加
+  root 'static_pages#home'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
