24886c88507be75db58108568b8ef9c1ad19bae1
baramatsubonzo <baramatsubonzo@gmail.com>
2018-09-07 11:43:51 +0900

    クレジットカード決済の追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   resources :products
+
+  # ↓ の3行を追加
+resources :products do
+  post :pay, on: :member
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
